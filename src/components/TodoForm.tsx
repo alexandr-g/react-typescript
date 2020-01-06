773d2c8014c4fb22a049d4a04227b2216237aa1d
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-01-06 19:52:13 +0100

    wip

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export const TodoForm: React.FC = () => {
+  //   const [title, setTitle] = useState<string>('')
+  const ref = useRef<HTMLInputElement>(null)
+
+  //   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //     setTitle(event.target.value)
+  //   }
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(ref.current!.value)
+      ref.current!.value = ''
+      //   console.log('pressed', title)
+      //   setTitle('')
+    }
+  }
+
   return (
     <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Enter todo title" />
+      <input
+        // onChange={handleChange}
+        // value={title}
+        ref={ref}
+        type="text"
+        id="title"
+        placeholder="Enter todo title"
+        onKeyPress={handleKeyPress}
+      />
       <label htmlFor="title" className="active">
         Enter todo title
       </label>
