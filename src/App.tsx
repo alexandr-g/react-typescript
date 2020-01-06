308c897c6b71fb4ffaabf90e35d9aa74a6b6f82c
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-01-06 17:50:33 +0100

    Render todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react'
-import Navbar from './components/Navbar'
+import { Navbar } from './components/Navbar'
+import { TodoForm } from './components/TodoForm'
 
 const App: React.FC = () => {
   return (
     <Fragment>
       <Navbar />
       <div className="container">
-        <h1>Test</h1>
+        <TodoForm />
       </div>
     </Fragment>
   )
