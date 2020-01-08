8c5a757d687af894e49abae97121c86ce7fe5406
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-01-08 13:15:47 +0100

    Create about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+export const About: React.FC = () => {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        possimus doloribus error cumque autem asperiores, ullam deserunt quidem
+        omnis doloremque itaque eius eaque sint facilis unde tenetur reiciendis
+        aliquam soluta?
+      </p>
+      <button className="btn">Go back</button>
+    </Fragment>
+  )
+}
