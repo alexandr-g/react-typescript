8c5187ec134dfc11e78a66f64751815c02e7daa9
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-01-08 13:16:17 +0100

    Configure react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,22 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { Todos } from './pages/Todos'
+import { About } from './pages/About'
 
 import { Navbar } from './components/Navbar'
-import { TodoForm } from './components/TodoForm'
-import { TodoList } from './components/TodoList'
-import { ITodo } from './interfaces'
-
-declare var confirm: (question: string) => boolean
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Array<ITodo>>([])
-
-  useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
-    setTodos(saved)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
-
-  const addHandler = (title: string) => {
-    const newTodo: ITodo = {
-      title,
-      id: Date.now(),
-      completed: false
-    }
-
-    setTodos(prev => [newTodo, ...prev])
-  }
-
-  const toggleHandler = (id: number) => {
-    setTodos(prev =>
-      prev.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed
-        }
-
-        return todo
-      })
-    )
-  }
-
-  const removeHandler = (id: number) => {
-    const shouldRemove = confirm('Are you sure you want to delete this todo?')
-
-    if (shouldRemove) {
-      setTodos(prev => prev.filter(todo => todo.id !== id))
-    }
-  }
-
   return (
-    <Fragment>
+    <BrowserRouter>
       <Navbar />
       <div className="container">
-        <TodoForm onAdd={addHandler} />
-
-        <TodoList
-          todos={todos}
-          onToggle={toggleHandler}
-          onRemove={removeHandler}
-        />
+        <Switch>
+          <Route path="/" component={Todos} exact />
+          <Route path="/about" component={About} />
+        </Switch>
       </div>
-    </Fragment>
+    </BrowserRouter>
   )
 }
 
