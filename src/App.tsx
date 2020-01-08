047e5aa0f979f270545045502e61b319b7f52461
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2020-01-08 12:34:36 +0100

    Save todos to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,13 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Array<ITodo>>([])
 
   useEffect(() => {
-    const saved = JSON.parse(localStorage.getItems('todos') || '[]') as ITodo[]
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
     setTodos(saved)
   }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addHandler = (title: string) => {
     const newTodo: ITodo = {
